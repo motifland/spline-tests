3e68189ccac21c96ac07d8ac34ea43bebc63c3e6
Michael Fester <michael.fester@gmail.com>
2023-05-15 13:46:18 -0700

    Animation tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,10 @@ import Spline from '@splinetool/react-spline';
 export default function Home() {
   return (
     <main>
+      <Spline scene="https://prod.spline.design/JjuAUS8iM07Bemju/scene.splinecode" />
       <div className="flex flex-row">
-        <div className="">
-          Enterprise-grade AI chatbots for your website and docs
-        </div>
-        <div className="h-[600px] w-[600px]">
-          <Spline scene="https://prod.spline.design/JjuAUS8iM07Bemju/scene.splinecode" />
-        </div>
+        <div>Message</div>
+        <div style={{ width: 600, height: 600 }}></div>
       </div>
     </main>
   );
