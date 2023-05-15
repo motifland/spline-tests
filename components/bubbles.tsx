8a3d6c22b899295d44fd8ab494b44b1b1e3e3287
Michael Fester <michael.fester@gmail.com>
2023-05-15 14:14:51 -0700

    Animation tests

---
+++ components/bubbles.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Bubbles() {
+  return (
+    <Spline
+      style={{ width: 600, height: 600 }}
+      scene="https://prod.spline.design/xAHz9Rr9aiFmbwD4/scene.splinecode"
+      // scene="https://prod.spline.design/JjuAUS8iM07Bemju/scene.splinecode"
+    />
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
+import dynamic from 'next/dynamic';
 import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-row">
-        <div>Message</div>
-        <div className="w-[600px] h-[600px]">
-          <Spline scene="https://prod.spline.design/JjuAUS8iM07Bemju/scene.splinecode" />
-        </div>
-      </div>
-    </main>
+    <Spline
+      className="w-[600px] h-[600px] animate-fade-in"
+      // scene="https://prod.spline.design/7Q6tWbXyV4mITSCs/scene.splinecode"
+      // scene="https://prod.spline.design/uUzsNaWpo0MUjzcd/scene.splinecode"
+      // scene="https://prod.spline.design/xAHz9Rr9aiFmbwD4/scene.splinecode"
+      scene="https://prod.spline.design/JjuAUS8iM07Bemju/scene.splinecode"
+    />
   );
 }
